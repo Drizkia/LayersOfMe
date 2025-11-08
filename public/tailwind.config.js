f4e08b1528f070ed4918f2b1a10304d5422375e8
Dimsky <dimasrizkia477@gmail.com>
2025-11-08 15:07:52 +0700

    Portofolio Static From Laravel

---
+++ public/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#9333ea",
+        accent: "#f59e0b",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
